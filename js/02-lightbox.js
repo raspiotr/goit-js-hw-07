d427b107d8ff3e5fae1684c45e43d3fb91c0daa9
raspiotr <raspiotr@gmail.com>
2023-09-04 15:04:13 +0200

    task 02 ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarkup = galleryItems.map((item) => {
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("gallery__image");
+  imgElement.setAttribute("src", item.preview);
+  imgElement.setAttribute("alt", item.description);
+  const linkElement = document.createElement("a");
+  linkElement.prepend(imgElement);
+  linkElement.classList.add("gallery__item");
+  linkElement.setAttribute("href", item.original);
+  const listElement = document.createElement("li");
+  listElement.prepend(linkElement);
+  return listElement;
+});
+
+const gallery = document.querySelector(".gallery");
+gallery.prepend(...galleryMarkup);
+
+const lightboxGallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
